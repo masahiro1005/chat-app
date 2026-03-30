bd95110529691d976fcc8da9c122c0c98aa96e78
masahiro1005 <masa.sub.hiro@gmail.com>
2026-03-30 22:07:09 +0900

    チャット画面の編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_03_29_010423) do
+ActiveRecord::Schema[7.1].define(version: 2026_03_30_125355) do
   create_table "room_users", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "room_id", null: false
     t.bigint "user_id", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_03_29_010423) do
   create_table "rooms", charset: "utf8mb3", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
